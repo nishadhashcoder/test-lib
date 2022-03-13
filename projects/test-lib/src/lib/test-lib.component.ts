c8871e195ee34689ff5a8b67832e935c72f20580
NISHAD <mohammed.nishad@capgemini.com>
2022-03-13 22:15:09 +0530

    test package

---
+++ projects/test-lib/src/lib/test-lib.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'lib-TestLib',
+  template: `
+    <p>
+      test-lib works!
+    </p>
+  `,
+  styles: [
+  ]
+})
+export class TestLibComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
